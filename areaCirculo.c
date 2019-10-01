ac406e18ca33373e008b2b412408705964ede89a
Xavier Tapia <55768585+XavierTapia@users.noreply.github.com>
2019-10-01 16:41:02 -0500

    Add files via upload

--- areaCirculo.c
+++ areaCirculo.c
@@ -1,13 +1,14 @@
 #include <stdio.h>                              //importar librerias
+#include <math.h>
 #define PI 3.1416                               //definicion de constantes
 
 int main(){                                     //declaracion de la funcion principal
     float radio,area;                           //declaracion de variables
     printf("Ingrese radio: ");                  //salida en pantalla
     scanf("%f",&radio);                         //lectura de la variable radio
-    area=PI*radio*radio;                        //proceso
+    //area=PI*radio*radio;                      proceso
+    area=PI*pow(radio,2);                       //proceso con math.h
     printf("El area es: %f\n\n",area);          //salida del valor de la variable area
     system("pause");                            //espera una tecla para continuar
-    system("cls");                              //limpia la pantalla
     return 0;
 }
